0334058128e314740ba0901b1881afb6d18b0d78
向往的生活 <hit_whpeng@163.com>
2020-11-04 22:57:47 +0800

    消费者体验代码完善

--- dubbo-consumer-service/src/main/java/com/ysdrzp/consumer/App.java
+++ dubbo-consumer-service/src/main/java/com/ysdrzp/consumer/App.java
@@ -6,18 +6,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.io.IOException;
 
 /**
- *
+ * 消费者启动类
+ * @author 向往的生活
  */
 public class App {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:dubbo-consumer.xml");
-        context.start();
-
         HelloService helloService = (HelloService) context.getBean("helloService");
-        helloService.sayHello("lee");
-        // 按任意键退出
-        System.in.read();
+        System.out.println("消费者启动...");
+        System.out.println(helloService.sayHello("lee"));
+        Thread.sleep(Long.MAX_VALUE);
     }
 
 }
